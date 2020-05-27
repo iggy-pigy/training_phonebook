f867be8d93ff24786a08091edfd1171ffad9318f
iggy-pigy <ilgakokorisa@gmail.com>
2020-05-27 17:26:10 +0100

    Addid window.confrim method

--- src/components/Person.js
+++ src/components/Person.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Person = (props) => {
     const { person, deleteFunc } = props
-    const handleDelete = () => { deleteFunc(person.id) }
+    //const handleDelete = () => { deleteFunc(person.id) }
     return (
         <li>
-            <h5> {person.name} {person.number} <button onClick={handleDelete}  >Delete</button></h5>
+            <h5> {person.name} {person.number} <button onClick={() => { if (window.confirm(`Delete ${person.name}?`)) { deleteFunc(person.id) }; }} >Delete</button></h5>
         </li>
     );
 };
