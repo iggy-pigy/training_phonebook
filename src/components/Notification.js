f21cd4c3276c9125fca71dc7f43a077e51fb1147
iggy-pigy <ilgakokorisa@gmail.com>
2020-05-28 13:13:55 +0100

    Added Inline Style to Notification component

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 function Notification({ message }) {
+    const messageStyle = {
+        color: 'green',
+        fontStyle: 'italic',
+        fontSize: 20,
+        background: 'lightGrey',
+        borderStyle: 'solid',
+        borderRadius: 5,
+        padding: 10,
+        marginBottom: 10
+    }
     if (message === null) {
         return null
     }
     return (
-        <div className="success">
+        <div style={messageStyle}>
             {message}
         </div>
     );
