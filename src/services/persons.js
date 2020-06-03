3cc7a9d6b5321c114639b4cfd4f0e1193cbaae94
iggy-pigy <ilgakokorisa@gmail.com>
2020-06-03 15:27:57 +0100

    Changed baseUrl

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+//const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'https://stark-fjord-63024.herokuapp.com/api/persons'
 
 const getAll = () => {
     //return axios.get(baseUrl)
